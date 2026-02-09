2b1e143d23617d80db80703d3e58e48b2e954820
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-02-09 14:32:02 +0200

    start addList ui

--- frontend/src/pages/BoardDetails/BoardDetails.jsx
+++ frontend/src/pages/BoardDetails/BoardDetails.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { BoardContext } from "../../context/BoardContext.jsx";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient , useMutation } from "@tanstack/react-query";
 import styles from "./BoardDetails.module.css";
 import Employees from "../../layout/Employees.jsx";
 import Lists from "../../layout/Lists.jsx";
 import { ListContext } from "../../context/ListContext.jsx";
 export default function BoardDetails() {
   const { id } = useParams();
+  const queryClient = useQueryClient();
   const { getBoardByItsId } = useContext(BoardContext);
   const [boardDetails, setBoardDetails] = useState(null);
-  let { getListsByBoardId } = useContext(ListContext);
+//   ----------------------------------------
+  let { getListsByBoardId , createList } = useContext(ListContext);
+//   --------------------------------------------
+  const [isAddingList, setIsAddingList] = useState(false);
+  const [listTitle, setListTitle] = useState("");
+
   const { data, error, isLoading } = useQuery({
     queryKey: ["boardDetails"],
     queryFn: () => getBoardByItsId(id),
