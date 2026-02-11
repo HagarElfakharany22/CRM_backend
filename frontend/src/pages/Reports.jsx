a1706d7d957cd3f7f8fd00ce18e93d0e87e6d532
HagarElfakharany22 <ahmedhagar293@gmail.com>
2026-02-11 12:02:04 +0200

    update attendace function and its design

--- frontend/src/pages/Reports.jsx
+++ frontend/src/pages/Reports.jsx
@@ -11,13 +11,57 @@ export default function Reports() {
   }, []);
 
   return (
-    <div>
-      <h2>Attendance Records</h2>
-      {attendance.map(a => (
-        <div key={a._id}>
-          <span>{a.user.name}</span> - <span>{a.loginAt}</span> - <span>{a.logoutAt || "Still logged in"}</span>
-        </div>
-      ))}
+    <div className='container-fluid py-4 bg-light min-vh-100'>
+      <h2 className='mb-1 fw-bold text-dark'> Attendance Records</h2>
+      <div className='card shadow-sm border-0 overflow-hide'>
+        <table className='table table-hover align middle mb-0'>
+          <style>
+            {`
+                .task-row {
+                  transition: background-color 0.2s;
+                }
+                .task-row:hover {
+                  background-color: #f8f9fa;
+                }
+                .task-row .view-icon {
+                  opacity: 0;
+                  transition: opacity 0.2s;
+                }
+                .task-row:hover .view-icon {
+                  opacity: 1;
+                }
+              `}
+          </style>
+          <thead className='bg-white border-bottom'>
+            <tr>
+              <th className='ps-4 py-3 text-uppercase small fw-bold text-muted'>Name</th>
+              <th className='ps-4 py-3 text-uppercase small fw-bold text-muted'>In</th>
+              <th className='ps-4 py-3 text-uppercase small fw-bold text-muted'>out</th>
+
+            </tr>
+          </thead>
+        <tbody>
+  {attendance.map(a => (
+    <tr key={a._id} className="task-row">
+      <td className='ps-4 py-3'>
+        {a.user?.name }
+      </td>
+
+      <td className='ps-4 py-3'>
+        {new Date(a.loginAt).toLocaleString()}
+      </td>
+
+      <td className='ps-4 py-3'>
+        {a.logoutAt
+          ? new Date(a.logoutAt).toLocaleString()
+          : "Still logged in"}
+      </td>
+    </tr>
+  ))}
+</tbody>
+      </table>
     </div>
+
+    </div >
   );
 }
