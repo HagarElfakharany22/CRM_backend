5d5210f9d82a422c4ef39a1cb86f1464b4c9d169
Hana20-Mohsen <hana2021mohsen@gmail.com>
2026-02-24 11:40:39 +0200

    end get Assigned tasks By Emp Id in back

--- backend/src/modules/assignedTasks/assignedTasks.controller.js
+++ backend/src/modules/assignedTasks/assignedTasks.controller.js
@@ -3,9 +3,11 @@ import { upload } from "../../utilities/multer/multer.js";
 import assignTask from "./services/assignTasks.service.js";
 import { authentication } from "../../middleware/auth.middleware.js";
 import getAllAssignedTasks from "./services/getAllAssignedTask.service.js";
+import getAssignedtasksByEmpId from "./services/getAssignedTasksByEmpId.service.js";
 const router=Router()
 
 
 router.post('/assign',authentication() ,upload.single("image"),assignTask)
 router.get('/all' , authentication() , getAllAssignedTasks)
+router.get('/byEmpId' , authentication() , getAssignedtasksByEmpId)
 export default router;
--- backend/src/modules/assignedTasks/services/getAssignedTasksByEmpId.service.js
+++ backend/src/modules/assignedTasks/services/getAssignedTasksByEmpId.service.js
@@ -1,8 +1,22 @@
 import AssignedTasksList from "../../../DB/models/assignedTasksList.model.js";
-import { asyncHandler } from "../../../utilities/error/error";
+import { asyncHandler } from "../../../utilities/error/error.js";
 
 const getAssignedtasksByEmpId=asyncHandler(async(req , res , next)=>{
     const id=req.user._id;
+    // console.log(`user id : ${id}`);
+    const assignedTasks= await AssignedTasksList.find({userId : id})
+    if(assignedTasks.length==0){
+        return res.status(404).json({
+            status:'fail',
+            message:'no assigned tasks found for this user'
+        })
+    }
+    return res.status(200).json({
+        status:'success',
+        message:'assigned tasks retrived successfully',
+        assignedTasks
+    })
+    
     
 })
 
